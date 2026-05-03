93edc0d48ff9d18607d2a706f6fb25674514f5a0
Sulaiman <240100743@ktech.edu.kw>
2026-05-03 23:21:14 +0300

    fix file formatting and postgres database

--- database.py
+++ database.py
@@ -1,45 +1,23 @@
 import os
-import sqlite3
 import psycopg2
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
-def using_postgres():
-    return DATABASE_URL is not None
-
 def connect_db():
-    if using_postgres():
-        return psycopg2.connect(DATABASE_URL)
-    return sqlite3.connect("system.db")
-
-def placeholder():
-    if using_postgres():
-        return "%s"
-    return "?"
+    return psycopg2.connect(DATABASE_URL)
 
 def create_table():
     conn = connect_db()
     cursor = conn.cursor()
 
-    if using_postgres():
-        cursor.execute("""
-        CREATE TABLE IF NOT EXISTS purchases (
-            id SERIAL PRIMARY KEY,
-            civil_id TEXT NOT NULL,
-            product TEXT NOT NULL,
-            date TEXT NOT NULL
-        )
-        """)
-    else:
-        cursor.execute("""
-        CREATE TABLE IF NOT EXISTS purchases (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            civil_id TEXT NOT NULL,
-            product TEXT NOT NULL,
-            date TEXT NOT NULL
-        )
-        """)
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS purchases (
+        id SERIAL PRIMARY KEY,
+        civil_id TEXT NOT NULL,
+        product TEXT NOT NULL,
+        date TEXT NOT NULL
+    )
+    """)
 
     conn.commit()
     conn.close()
-    
